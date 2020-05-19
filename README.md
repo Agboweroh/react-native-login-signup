3140a0ffb9a0bf555f25c2e0d0b402b0a79b8afc
Jacob Momoh <jacobmomoh20@gmail.com>
2020-05-19 15:58:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Working with styled components and react navigation
+
+To run the app :
+Enter the project directory and enter   --- 'yarn install or npm install' on your terminal to install all dependencies.
+make sure you have yarn and node installed on your local machine.
+Then run 'react-native run-android
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,27 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-alert */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const SettingsScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Settings Screen</Text>
+        <Button
+          title="Click Here"
+          onPress={() => alert('Button Clicked!')}
+        />
+      </View>
+    );
+};
+
+export default SettingsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
